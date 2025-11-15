feff60eb7521716e28856f82cafc506217c3766a
Jatin <jatin8872648396@gmail.com>
2025-11-15 09:59:24 +0530

    testing worker deployement on railway

--- server/controller/Contact-Controller.js
+++ server/controller/Contact-Controller.js
@@ -1,6 +1,8 @@
 import Contact from "../models/contact-model.js";
 import User from "../models/user-model.js";
 import { emailQueue } from "../queues/emailQueue.js";
+import { clearCache } from "../utils/clearCache.js";
+import redis from "../utils/redis.js";
 
 //CONTACT PAGE
 export const contactController = async (req, res) => {
@@ -55,19 +57,36 @@ export const contactController = async (req, res) => {
 
     await emailQueue.add("contactFormSubmission", { mailOptions: mailOption });
 
+    await clearCache(["talentNest:approvedContacts"]);
+
     return res.status(200).json({
       MESSAGE: "Your message has been sent successfully!",
       SUCCESS: true,
     });
   } catch (error) {
     console.log(error);
-
     res.status(500).json({ MESSAGE: "Server error", SUCCESS: false });
   }
 };
 
+// Website reviews
 export const getAllContacts = async (req, res) => {
   try {
+    const cacheKey = "talentNest:approvedContacts";
+
+    try {
+      const reviews = await redis.get(cacheKey);
+      if (reviews) {
+        return res.status(200).json({
+          MESSAGE: "Contacts found",
+          allContacts: JSON.parse(reviews),
+          SUCCESS: true,
+        });
+      }
+    } catch (error) {
+      console.error("Redis error:", error?.message);
+    }
+
     const allContacts = await Contact.find({ isApproved: true })
       .populate("userID", "username profile.profilePhoto role")
       .sort({ createdAt: -1 })
@@ -79,6 +98,7 @@ export const getAllContacts = async (req, res) => {
         SUCCESS: false,
       });
     }
+    await redis.set(cacheKey, JSON.stringify(allContacts), "EX", 3600);
 
     return res.status(200).json({
       MESSAGE: "Contacts found",
